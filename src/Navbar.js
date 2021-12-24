a4596ce996ea3d832006b4c19398b1f14b86c395
mkashifsiddiqu <boykashif3@gmail.com>
2021-12-24 20:35:33 +0500

    simpleReactblog

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
-        <nav className="navbar">
+        <nav className="navbar" >
             <h1>DJ-blog</h1>
             <div className="link">
                 <Link to="/">Home</Link>
                 <Link to="/create">New blog</Link>
+                <Link to="/">Histroy</Link>
             </div>
         </nav>
      );
